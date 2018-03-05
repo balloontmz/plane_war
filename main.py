d090155f758715585492885e8d0b63be88863d34
tomtiddler <15111171986@163.com>
2018-03-05 22:40:41 +0800

    飞机大战主文件

---
+++ main.py
@@ -0,0 +1,18 @@
+#main.py
+import pygame
+import sys
+import traceback
+import myplane
+import enemy
+import bullet
+import supply
+
+
+from pygame.locals import *
+from random import *
+
+pygame.init()
+pygame.mixer.init()
+
+bg_size=width,height=480,700
+screen=
